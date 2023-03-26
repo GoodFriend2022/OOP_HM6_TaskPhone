eab3aa51127a41f5d39c35d47077527bbfb24457
Uri_Klevcov <a-08-137.mail.ru>
2023-03-26 14:38:33 +0600

    calling

--- Phone.java
+++ Phone.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public abstract class Phone implements Calling {
+
+    String name;
+    String userPlace;
+
+    public Phone() {
+        this.name = "телефон";
+        this.userPlace = "там где есть телефон";
+    }
 
     @Override
     public void calling() {
-        System.out.println("Внимание, Вам звонят!");
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите номер телефона >> \n");
+        int number = iScanner.nextInt();
+        iScanner.close();
+        System.out.printf("Идет звонок по номеру: %s ...", number);
     }
     
 }
