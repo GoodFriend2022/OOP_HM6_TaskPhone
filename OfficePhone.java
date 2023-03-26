fb099f263afa54c9c88c0bc8fec273f54b5f0ade
Uri_Klevcov <a-08-137.mail.ru>
2023-03-26 14:04:51 +0600

    start programm

---
+++ OfficePhone.java
@@ -0,0 +1,3 @@
+public class OfficePhone extends HomePhone {
+    
+}
---
+++ Phone.java
@@ -0,0 +1,8 @@
+public abstract class Phone implements Calling {
+
+    @Override
+    public void calling() {
+        System.out.println("Внимание, Вам звонят!");
+    }
+    
+}
